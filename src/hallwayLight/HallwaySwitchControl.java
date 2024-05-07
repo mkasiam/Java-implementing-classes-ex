ae05189f02d265e2fa7f432978f090444ed0d3e5
Kawsar Ahmmed <kawsarahmmed10001@gmail.com>
2024-05-07 11:48:57 +0600

    Hallway Switch Control New Classes added

---
+++ src/hallwayLight/HallwaySwitchControl.java
@@ -0,0 +1,45 @@
+package hallwayLight;
+
+public class HallwaySwitchControl {
+
+    private int firstSwitchState, secondSwitchState, lampState;
+
+    public HallwaySwitchControl() {
+        firstSwitchState = 0;
+        secondSwitchState = 0;
+    }
+
+    public int getFirstSwitchState() {
+        return firstSwitchState;
+    }
+
+    public int getSecondSwitchState() {
+        return secondSwitchState;
+    }
+
+    public int getSwitchState(int switchNum) {
+        if (switchNum == 1){
+            return firstSwitchState;
+        } else if (switchNum == 2) {
+            return secondSwitchState;
+        }
+    }
+
+    public int getLampState() {
+        return lampState;
+    }
+
+    public void toggleSwitch(int switchNum) {
+        if (switchNum == 1){
+            firstSwitchState = firstSwitchState ^ 1;
+        } else if (switchNum == 2) {
+            secondSwitchState = secondSwitchState ^ 1;
+        }
+        updateLampState();
+    }
+
+    public void updateLampState() {
+        lampState = firstSwitchState ^ secondSwitchState;
+    }
+
+}
